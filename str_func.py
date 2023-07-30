4ca74994ee78a659980e3fd030a563353ba99316
AlexBakushin <bakushin35@gmail.com>
2023-07-30 09:18:15 +0500

    add function title

--- str_func.py
+++ str_func.py
@@ -2,3 +2,7 @@ def upping(str_):
     """Converts the str to uppercase"""
     return str_.upper()
 
+
+def capital(str_):
+    """Makes the first letter of each word capitalized"""
+    return str_.title()
